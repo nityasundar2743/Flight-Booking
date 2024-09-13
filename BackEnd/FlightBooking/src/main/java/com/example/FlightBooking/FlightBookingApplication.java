171dcdf6924f87faed555c098ccb0ec18ed58434
nityasundar2743 <nityasundar2743@gmail.com>
2024-09-13 17:40:32 +0530

    First Commit

---
+++ BackEnd/FlightBooking/src/main/java/com/example/FlightBooking/FlightBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlightBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingApplication.class, args);
+	}
+
+}
---
+++ BackEnd/FlightBooking/src/test/java/com/example/FlightBooking/FlightBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlightBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
