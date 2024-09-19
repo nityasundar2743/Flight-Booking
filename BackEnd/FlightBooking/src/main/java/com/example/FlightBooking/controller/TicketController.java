7c4f4685facdfed8bbb59301b8f783c9cc49a0ed
nityasundar2743 <nityasundar2743@gmail.com>
2024-09-19 14:34:16 +0530

    Cors Error Fixed

--- BackEnd/FlightBooking/src/main/java/com/example/FlightBooking/controller/TicketController.java
+++ BackEnd/FlightBooking/src/main/java/com/example/FlightBooking/controller/TicketController.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/ticket")
 public class TicketController {
 
--- BackEnd/FlightBooking/src/main/java/com/example/FlightBooking/controller/UserController.java
+++ BackEnd/FlightBooking/src/main/java/com/example/FlightBooking/controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/user")
 public class UserController {
 
